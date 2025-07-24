a8241cb1ada45bc48f1e370da55d8eab961424a2
lmgorman <kiwielemgee@gmail.com>
2025-07-24 11:42:41 +0100

    Update convert_gbff_togff.sh

--- scripts/bioinformatics/convert_gbff_togff.sh
+++ scripts/bioinformatics/convert_gbff_togff.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=Ahya_align_star
+#SBATCH --nodes=1 --cpus-per-task=8
+#SBATCH --mem=250G  # Requested Memory
+#SBATCH -p gpu  # Partition
+#SBATCH -G 1  # Number of GPUs
+#SBATCH --time=24:00:00  # Job time limit
+#SBATCH -o slurm-%j.out  # %j = job ID
+#SBATCH -D work/pi_hputnam_uri_edu/20250107_COTS_LG/
+
+#load modules
+echo "Loading programs" $(date)
+module load uri/main
+module load module load genometools/1.6.5
 
