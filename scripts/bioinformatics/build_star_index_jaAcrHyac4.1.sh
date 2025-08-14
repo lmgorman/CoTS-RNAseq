f3f5bd3ccce805998d8acf82486f79d007edb387
lmgorman <kiwielemgee@gmail.com>
2025-08-14 14:40:44 +0100

    Update build_star_index_jaAcrHyac4.1.sh

--- scripts/bioinformatics/build_star_index_jaAcrHyac4.1.sh
+++ scripts/bioinformatics/build_star_index_jaAcrHyac4.1.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=STAR_genome_index_Ahya
 #SBATCH --nodes=1 --cpus-per-task=8
 #SBATCH --mem=250G  # Requested Memory
-#SBATCH -p gpu  # Partition
-#SBATCH -G 1  # Number of GPUs
+#SBATCH -p uri-cpu
 #SBATCH --time=24:00:00  # Job time limit
 #SBATCH -o slurm-%j.out  # %j = job ID
 #SBATCH -D /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/acr_hya_jaAcrHyac4.1
@@ -13,18 +12,22 @@ module load uri/main
 module load STAR/2.7.11b-GCC-12.3.0
 module load gffread/0.12.7
 
+echo "[$(date)] Converting GFF3 to GTF"
+gffread /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/GCA_964291705.1_jaAcrHyac4.1_genomic.gbff.gff \
+  -T -o GCA_964291705.1_jaAcrHyac4.1_genomic.gtf
+
 echo "[$(date)] Starting STAR genome index build with GFF3 annotation"
 
 # Variables
 GENOME_FA="GCA_964291705.1_jaAcrHyac4.1_genomic.fna"
-GTF_FILE="GCA_964291705.1_jaAcrHyac4.1_genomic.gff3"
+GTF_FILE="GCA_964291705.1_jaAcrHyac4.1_genomic.gtf"
 INDEX_DIR="star_index_jaAcrHyac4.1"
 
 # Create output directory
 mkdir -p "$INDEX_DIR"
 
 # Run STAR genomeGenerate with annotation
-STAR --runThreadN 6 \
+STAR --runThreadN 8 \
     --runMode genomeGenerate \
     --genomeDir "$INDEX_DIR" \
     --genomeFastaFiles "$GENOME_FA" \
