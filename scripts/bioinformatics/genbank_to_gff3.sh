a9257acda62f5b46fd1090c09dbb92136557cebd
lmgorman <kiwielemgee@gmail.com>
2025-08-14 14:27:27 +0100

    Update genbank_to_gff3.sh

--- scripts/bioinformatics/genbank_to_gff3.sh
+++ scripts/bioinformatics/genbank_to_gff3.sh
@@ -8,10 +8,7 @@
 #SBATCH -o slurm-%j.out  # %j = job ID
 #SBATCH -D /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman
 
-# Clone github repo
-git clone https://github.com/bioperl/bioperl-live.git
-
 #Run bioperl
 module load uri/main
 module load BioPerl/1.7.8-GCCcore-13.3.0
-perl /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/bin/bp_genbank2gff3 /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/acr_hya_jaAcrHyac4.1/GCA_964291705.1_jaAcrHyac4.1_genomic.gbff
+perl /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/bioperl-live/bin/bp_genbank2gff3 /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/acr_hya_jaAcrHyac4.1/GCA_964291705.1_jaAcrHyac4.1_genomic.gbff
