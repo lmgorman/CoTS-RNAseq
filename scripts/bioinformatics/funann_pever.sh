0fd084e56613973d47fe8857362ff87ec15a3d4c
lmgorman <kiwielemgee@gmail.com>
2025-10-05 13:18:30 +0100

    Update funann_pever.sh

--- scripts/bioinformatics/funann_pever.sh
+++ scripts/bioinformatics/funann_pever.sh
@@ -27,7 +27,6 @@ FUNANNOTATE_SIF="/modules/opt/linux-ubuntu24.04-x86_64/funannotate/1.8.17/funann
 # Copy input data to scratch
 echo "[$(date)] Copying input data to scratch..."
 cp -r /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/refs/por-ever/Porites_evermanni_v1_clean.annot.pep.fa $SCRATCHDIR
-cp -r /home/lucy_gorman_uri_edu/scripts/fixed.gff $SCRATCHDIR
 
 # Copy interpro and eggnog files to scratch (optional but recommended for full scratch use)
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/interpro/output/Porites_evermanni_v1_clean.annot.pep.fa.xml $SCRATCHDIR/iprscan.xml
@@ -36,7 +35,7 @@ cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/eggnog/pever_eggnog.emapp
 # Run funannotate inside Apptainer from scratch
 echo "[$(date)] Starting funannotate..."
 apptainer run "$FUNANNOTATE_SIF" funannotate annotate \
-  --gff $SCRATCHDIR/fixed.gff \
+  --gff $SCRATCHDIR/fixed_with_header.gff \
   --fasta $SCRATCHDIR/Porites_evermanni_v1_clean.annot.pep.fa \
   -s "Porites evermanni" \
   -o $SCRATCHDIR/output \
