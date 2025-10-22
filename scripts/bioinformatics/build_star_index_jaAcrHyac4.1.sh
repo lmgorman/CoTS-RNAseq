93292551f509dcd50ce79353d2a7293978764470
lmgorman <kiwielemgee@gmail.com>
2025-10-22 10:49:12 +0100

    Update build_star_index_jaAcrHyac4.1.sh

--- scripts/bioinformatics/build_star_index_jaAcrHyac4.1.sh
+++ scripts/bioinformatics/build_star_index_jaAcrHyac4.1.sh
@@ -14,18 +14,25 @@ module load uri/main
 module load STAR/2.7.11b-GCC-12.3.0
 module load gffread/0.12.7
 
-# Set custom scratch directory
-SCRATCHDIR=/scratch3/workspace/lucy_gorman_uri_edu-lucyscratch/star_index_jaAcrHyac4.1
-
 echo "[$(date)] Converting GFF3 to GTF"
 gffread /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/GCA_964291705.1_jaAcrHyac4.1_genomic.gbff.gff \
   -T -o GCA_964291705.1_jaAcrHyac4.1_genomic.gtf
 
 echo "[$(date)] Starting STAR genome index build in scratch: $SCRATCHDIR"
 
-# Variables
-GENOME_FA="GCA_964291705.1_jaAcrHyac4.1_genomic.fna"
-GTF_FILE="GCA_964291705.1_jaAcrHyac4.1_genomic.gtf"
+# Set custom scratch directory
+SCRATCHDIR=/scratch3/workspace/lucy_gorman_uri_edu-lucyscratch/star_index_jaAcrHyac4.1
+
+mkdir -p "$SCRATCHDIR"
+
+# Copy genome FASTA and GTF to scratch
+echo "[$(date)] Copying genome FASTA and GTF to scratch"
+cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/acr_hya_jaAcrHyac4.1/GCA_964291705.1_jaAcrHyac4.1_genomic.fna "$SCRATCHDIR"
+cp GCA_964291705.1_jaAcrHyac4.1_genomic.gtf "$SCRATCHDIR"
+
+# Set variables to point to scratch versions
+GENOME_FA="$SCRATCHDIR/GCA_964291705.1_jaAcrHyac4.1_genomic.fna"
+GTF_FILE="$SCRATCHDIR/GCA_964291705.1_jaAcrHyac4.1_genomic.gtf"
 INDEX_DIR="$SCRATCHDIR"
 
 # Run STAR genomeGenerate
@@ -37,10 +44,15 @@ STAR --runThreadN 8 \
     --genomeSAindexNbases 13 \
     --sjdbOverhang 99
 
-# Copy STAR index back to permanent storage
+# Check if STAR index was created before copying
 FINAL_OUTPUT_DIR="/work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/star_index_jaAcrHyac4.1"
 mkdir -p "$FINAL_OUTPUT_DIR"
-cp -r "$INDEX_DIR"/* "$FINAL_OUTPUT_DIR"
 
-echo "[$(date)] Genome index build completed and copied to $FINAL_OUTPUT_DIR"
+if [ -f "$INDEX_DIR/Genome" ]; then
+    cp -r "$INDEX_DIR"/* "$FINAL_OUTPUT_DIR"
+    echo "[$(date)] Genome index build completed and copied to $FINAL_OUTPUT_DIR"
+else
+    echo "[$(date)] ERROR: STAR index not created. Check STAR logs." >&2
+    exit 1
+fi
 
