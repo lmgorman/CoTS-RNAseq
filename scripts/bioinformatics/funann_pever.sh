02c0887b82368ba2bdc380c06133d261ce88e011
lmgorman <kiwielemgee@gmail.com>
2025-10-27 21:55:10 +0000

    Update funann_pever.sh

--- scripts/bioinformatics/funann_pever.sh
+++ scripts/bioinformatics/funann_pever.sh
@@ -26,7 +26,6 @@ FUNANNOTATE_SIF="/modules/opt/linux-ubuntu24.04-x86_64/funannotate/1.8.17/funann
 
 # Copy input data to scratch
 echo "[$(date)] Copying input data to scratch..."
-cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/refs/por-ever/Porites_evermanni_v1.fa $SCRATCHDIR
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/interpro/output/Porites_evermanni_v1_clean.annot.pep.fa.xml $SCRATCHDIR/iprscan.xml
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/eggnog/pever_eggnog.emapper.annotations $SCRATCHDIR/eggnog.annotations
 
@@ -34,7 +33,7 @@ cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/eggnog/pever_eggnog.emapp
 echo "[$(date)] Starting Funannotate annotation..."
 apptainer run "$FUNANNOTATE_SIF" funannotate annotate \
   --gff $SCRATCHDIR/Porites_evermanni_v1_FIXED.short.gff \
-  --fasta $SCRATCHDIR/Porites_evermanni_v1.fa \
+  --fasta $SCRATCHDIR/Porites_evermanni_v1.short.fa \
   -s "Porites evermanni" \
   -o $SCRATCHDIR/output \
   --iprscan $SCRATCHDIR/iprscan.xml \
