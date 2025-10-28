aeec2bbc379883a1e56e4ec6a55654d69020de4e
lmgorman <kiwielemgee@gmail.com>
2025-10-28 16:51:25 +0000

    Update funann_pever.sh

--- scripts/bioinformatics/funann_pever.sh
+++ scripts/bioinformatics/funann_pever.sh
@@ -28,15 +28,20 @@ module load funannotate/1.8.17
 # Define Apptainer container
 FUNANNOTATE_SIF="/modules/opt/linux-ubuntu24.04-x86_64/funannotate/1.8.17/funannotate-1.8.17.sif"
 
+
+
 # Copy input data to scratch
 echo "[$(date)] Copying input data to scratch..."
+cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/refs/por-ever/Porites_evermanni_v1.fa $SCRATCHDIR
+cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por-ever/Porites_evermanni_v1_FIXED.gff $SCRATCHDIR
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/interpro/output/Porites_evermanni_v1_clean.annot.pep.fa.xml $SCRATCHDIR/iprscan.xml
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/eggnog/pever_eggnog.emapper.annotations $SCRATCHDIR/eggnog.annotations
 
 echo "[$(date)] Starting Funannotate annotation..."
 apptainer run "$FUNANNOTATE_SIF" funannotate annotate \
-  -g $SCRATCHDIR/pever.gbk \
-  -o $SCRATCHDIR/output \
+  --gff $SCRATCHDIR/Porites_evermanni_v1_FIXED.gff \
+  --fasta $SCRATCHDIR/Porites_evermanni_v1.fa \
+  -o $SCRATCHDIR/output
   --iprscan $SCRATCHDIR/iprscan.xml \
   --eggnog $SCRATCHDIR/eggnog.annotations \
   --busco_db metazoa \
