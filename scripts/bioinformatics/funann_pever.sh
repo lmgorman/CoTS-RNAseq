91825eb32080a272d2508f8604f09d47ba9e1566
lmgorman <kiwielemgee@gmail.com>
2025-10-28 16:51:33 +0000

    Update funann_pever.sh

--- scripts/bioinformatics/funann_pever.sh
+++ scripts/bioinformatics/funann_pever.sh
@@ -28,8 +28,6 @@ module load funannotate/1.8.17
 # Define Apptainer container
 FUNANNOTATE_SIF="/modules/opt/linux-ubuntu24.04-x86_64/funannotate/1.8.17/funannotate-1.8.17.sif"
 
-
-
 # Copy input data to scratch
 echo "[$(date)] Copying input data to scratch..."
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/refs/por-ever/Porites_evermanni_v1.fa $SCRATCHDIR
