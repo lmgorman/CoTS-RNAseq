c1daf0092ce0ccc730336eec170a1aa9f92c078a
lmgorman <kiwielemgee@gmail.com>
2025-11-02 19:28:08 +0000

    Update funann_pever_trunc.sh

--- scripts/bioinformatics/funann_pever_trunc.sh
+++ scripts/bioinformatics/funann_pever_trunc.sh
@@ -32,7 +32,7 @@ FUNANNOTATE_SIF="/modules/opt/linux-ubuntu24.04-x86_64/funannotate/1.8.17/funann
 echo "[$(date)] Copying input data to scratch..."
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/interpro/output/Porites_evermanni_v1_clean.annot.pep.fa.xml $SCRATCHDIR/iprscan.xml
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por/eggnog/pever_eggnog.emapper.annotations $SCRATCHDIR/eggnog.annotations
-cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/refs/por-ever/Porites_evermanni_v1_clean_fixed.annot.pep.fa $SCRATCHDIR/Porites_evermanni_v1_clean_fixed.annot.pep.fa #wants genome
+cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/refs/por-ever/Porites_evermanni_v1.fa  $SCRATCHDIR/Porites_evermanni_v1.fa
 cp /work/pi_hputnam_uri_edu/ashuffmyer/cots-gorman/por-ever/Porites_evermanni_v1_FIXED.gff $SCRATCHDIR/Porites_evermanni_v1_FIXED.gff
 
 # Ensure FASTA and GFF3 exist in scratch
@@ -44,7 +44,7 @@ fi
 echo "[$(date)] Starting Funannotate annotation..."
 apptainer run --bind $SCRATCHDIR:$SCRATCHDIR "$FUNANNOTATE_SIF" funannotate annotate \
   --gff $SCRATCHDIR/Porites_evermanni_v1_FIXED.gff \
-  --fasta $SCRATCHDIR/Porites_evermanni_v1_clean_fixed.annot.pep.fa \ #wants genome
+  --fasta $SCRATCHDIR/Porites_evermanni_v1.fa
   -s "Porites evermanni" \
   -o $SCRATCHDIR/output \
   --iprscan $SCRATCHDIR/iprscan.xml \
