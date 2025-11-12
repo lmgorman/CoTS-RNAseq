9505ecbdc82c1cc755757988d1d78b11487c478f
lmgorman <kiwielemgee@gmail.com>
2025-11-12 21:12:30 +0000

    Update toxcodan_paus.sh

--- ToxCodAn/toxcodan_paus.sh
+++ ToxCodAn/toxcodan_paus.sh
@@ -6,16 +6,22 @@
 #SBATCH --time=30-00:00:00
 #SBATCH --constraint=avx512
 #SBATCH -p uri-cpu
-#SBATCH -o toxcodan-paus-%j.out
-#SBATCH -e toxcodan-paus-%j.error
+#SBATCH -o /work/pi_hputnam_uri_edu/lgorman/ToxCodAn-Genome/logs/toxcodan-paus-%j.out
+#SBATCH -e /work/pi_hputnam_uri_edu/lgorman/ToxCodAn-Genome/logs/toxcodan-paus-%j.error
 
+# Ensure Conda uses /work directories
+export CONDA_PKGS_DIRS=/work/pi_hputnam_uri_edu/lgorman/.conda/pkgs
+export CONDA_ENVS_DIRS=/work/pi_hputnam_uri_edu/lgorman/.conda/envs
+
+# -----------------------------
 # Load modules
+# -----------------------------
 module load uri/main
 module load conda/latest
 
-# Initialize Conda in batch
-source ~/miniforge3/etc/profile.d/conda.sh
+# Activate environment
 conda activate ToxcodanGenome
+
 
 # Run script
 python /work/pi_hputnam_uri_edu/lgorman/ToxCodAn-Genome/bin/toxcodan-genome.py \
