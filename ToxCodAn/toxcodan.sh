52145e137e0de6a010a6cc4e20536edff3e1d324
lmgorman <kiwielemgee@gmail.com>
2025-11-12 19:52:58 +0000

    Update toxcodan.sh

--- ToxCodAn/toxcodan.sh
+++ ToxCodAn/toxcodan.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=toxcodan-pever
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=100G
+#SBATCH --time=30-00:00:00
+#SBATCH --constraint=avx512
+#SBATCH -p uri-cpu
+#SBATCH -o toxcodan-pever-%j.out
+#SBATCH -e toxcodan-pever-%j.error
 
+module load conda
 conda activate ToxcodanGenome
 
