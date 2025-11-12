02539177fe4441b4eeca714dcc46a93e14534b0b
lmgorman <kiwielemgee@gmail.com>
2025-11-12 19:26:59 +0000

    Update toxcodan.sh

--- ToxCodAn/toxcodan.sh
+++ ToxCodAn/toxcodan.sh
@@ -1,14 +1,7 @@
 #Create conda environment in scratch directory in an interactive session
-cd /scratch/workspace/lucy_gorman_uri_edu-lucyscratch
 salloc -p cpu -c 2 --mem=5G --time 02:00:00
-module load conda/latest
-conda create --name Lucy python=3.7
-conda activate Lucy
+conda activate ToxcodanGenome
 
-Tip: First, ensure that you have added all conda channels properly in the following order:
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
 Create the environment:
 conda create -n ToxcodanGenome -c bioconda python biopython pandas blast exonerate miniprot gffread hisat2 samtools stringtie trinity spades
 Git clone the ToxCodAn-Genome repository and add the bin to your PATH:
